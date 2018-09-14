aaffad367e0ad60694f116a29b1351b80eb53aaf
jdrumgoole <joe@joedrumgoole.com>
2018-09-14 09:25:15 +0100

    Clean repo with working code

---
+++ curlpause.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -i -u "joe@joedrumgoole.com:f950d942-ae20-4b9e-b05d-903efb62f88c" --digest -H "Content-Type: application/json" -X PATCH "https://cloud.mongodb.com/api/atlas/v1.0/groups/5b9a96abc0c6e33ce9399e69/clusters/$1" --data '{"paused": true}'
